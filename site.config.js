ade5c7355af9615ab708b1e99bb5495b09c94135
knightofshadow <38887538+knightofshadow@users.noreply.github.com>
2022-01-10 21:24:23 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,16 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '159c01ebe0be48b4aecf0e67072a1222',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Blog of Outsider 47',
+  domain: 'https://135791.notion.site/Blog-159c01ebe0be48b4aecf0e67072a1222
+',
+  author: 'Outsider 47',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +18,9 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  //twitter: 'transitive_bs',
+  //github: 'transitive-bullshit',
+  //linkedin: 'fisch2',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
